9641dfb1a6cf73b3290483e3a19f8eaa75ee156b
Chirayu Kapoor <dev.csociety@gmail.com>
2023-08-01 01:00:42 +0530

    Added the Java file, tomcat config and maven config

---
+++ src/main/java/com/mycompany/app/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HelloWorldServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Hello, World!</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
